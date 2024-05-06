3007bcab8e06b0eb2f270983e0c7000b4d3e10a0
ianniccariluca <luca.ianniccari@itsrizzoli.it>
2024-05-06 12:31:18 +0200

    commit 3

--- script.js
+++ script.js
@@ -8,3 +8,42 @@ function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add('active');
 }
+document.addEventListener("DOMContentLoaded", function() {
+  const prevButton = document.getElementById('prevButton');
+  const nextButton = document.getElementById('nextButton');
+  const carosello = document.querySelector('.carosello-item-seriea');
+
+  let currentIndex = 0;
+  const slides = carosello.querySelectorAll('img');
+  const totalSlides = slides.length;
+
+
+  function nextSlide() {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlide();
+  }
+
+ 
+  function prevSlide() {
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      updateSlide();
+  }
+
+
+  function updateSlide() {
+      slides.forEach((slide, index) => {
+          if (index === currentIndex) {
+              slide.style.display = 'block';
+          } else {
+              slide.style.display = 'none';
+          }
+      });
+  }
+
+  
+  prevButton.addEventListener('click', prevSlide);
+  nextButton.addEventListener('click', nextSlide);
+
+ 
+  updateSlide();
+});
